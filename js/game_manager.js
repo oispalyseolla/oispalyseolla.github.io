3a04e80f33826d18296578d0b8040094d9ed826f
symbioosi <106813595+symbioosi@users.noreply.github.com>
2022-10-18 21:14:10 +0300

    Added Victory Sound

--- js/game_manager.js
+++ js/game_manager.js
@@ -60,6 +60,8 @@ GameManager.prototype.goKatko = function () {
 
 GameManager.prototype.isGameTerminated = function () {
   if (this.over || (this.won && !this.keepPlaying)) {
+    snd = new Audio("https://www.oispalyseolla.tk/snd/victory.mp3");
+    snd.play();
     return true;
   } else {
     return false;
